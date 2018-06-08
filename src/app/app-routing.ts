521f7d8957d9769a22033934cc5b2cf89fb6c73f
jared <jaredspecjr@gmail.com>
2018-06-08 09:32:58 -0700

    finish routing of all pages and set up a navbar using scss

--- src/app/app-routing.ts
+++ src/app/app-routing.ts
@@ -2,6 +2,13 @@ import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomePageComponent } from './home-page/home-page.component';
 import { LocationsComponent } from './locations/locations.component';
+import { PlansComponent } from './plans/plans.component';
+import { CoffeeComponent } from './coffee/coffee.component';
+import { ActivitiesComponent } from './activities/activities.component';
+import { PrintingComponent } from './printing/printing.component';
+import { WifiComponent } from './wifi/wifi.component';
+import { WorkSpaceComponent } from './work-space/work-space.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 const appRoutes: Routes = [
   {
@@ -11,6 +18,34 @@ const appRoutes: Routes = [
   {
     path: 'locations',
     component: LocationsComponent
+  },
+  {
+    path: 'plans',
+    component: PlansComponent
+  },
+  {
+    path: 'coffee',
+    component: CoffeeComponent
+  },
+  {
+    path: 'activities',
+    component: ActivitiesComponent
+  },
+  {
+    path: 'printing',
+    component: PrintingComponent
+  },
+  {
+    path: 'wifi',
+    component: WifiComponent
+  },
+  {
+    path: 'work-space',
+    component: WorkSpaceComponent
+  },
+  {
+    path: 'contact-us',
+    component: ContactUsComponent
   }
 ];
 
